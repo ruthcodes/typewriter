804ac41bb1ed02605603bfd2eb57205034f21e5d
Ruth Ashleigh <notruth@notruth.com>
2018-10-19 17:48:18 -0700

    Switch to dynamically generated keys

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-const keys = [...document.querySelectorAll('.key')];
 
+const one = document.querySelector('#rowOne');
+const two = document.querySelector('#rowTwo');
+const three = document.querySelector('#rowThree');
+
+const rowOne = [
+  {'one': 1}, {'two': 2}, {'three': 3}, {'four': 4}, {'five': 5},
+   {'six': 6}, {'seven': 7}, {'eight': 8}, {'nine': 9}, {'zero': 0}
+];
+
+const rowTwo = ['q','w','e','r','t','y','u','i','o','p'];
+
+const rowThree = [
+  {'z': 'z'}, {'x':'x'}, {'c':'c'}, {'v':'v'}, {'b':'b'}, {'n':'n'},
+  {'m':'m'}, {'comma': ','}, {'dot': '.'}, {'question': '?'}
+];
+
+const generateKeysFromObj = (row, element) => {
+  row.forEach(key => {
+    element.innerHTML += `
+      <div class="key" id=${Object.keys(key)}>
+        ${key[Object.keys(key)]}
+        <div class="key-stem"></div>
+      </div>
+    `
+  })
+}
+
+generateKeysFromObj(rowOne, one);
+generateKeysFromObj(rowThree, three);
+
+rowTwo.forEach(key => {
+  two.innerHTML += `
+    <div class="key" id=${key}>
+      ${key}
+      <div class="key-stem"></div>
+    </div>
+  `
+})
+
+
+
+
+/*
 keys.forEach(key => {
   key.addEventListener('click', function(){
     let currentTop = document.getElementById(this.id);
@@ -7,3 +49,5 @@ keys.forEach(key => {
     setTimeout(()=> currentTop.classList.remove('moveKeyRow'), 400);
   })
 })
+
+*/
