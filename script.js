707701b054b7c2e22a68d151110c196fe87dfe74
Ruth Ashleigh <notruth@notruth.com>
2018-10-20 17:32:14 -0700

    Switch key generator to arrays instead of objects.Add key click handlers

--- script.js
+++ script.js
@@ -1,60 +1,40 @@
-
+let canClick = true;
 const one = document.querySelector('#rowOne');
 const two = document.querySelector('#rowTwo');
 const three = document.querySelector('#rowThree');
 
-const rowOne = [
-  {'one': 1}, {'two': 2}, {'three': 3}, {'four': 4}, {'five': 5},
-   {'six': 6}, {'seven': 7}, {'eight': 8}, {'nine': 9}, {'zero': 0}
-];
-
+const rowOne = [1,2,3,4,5,6,7,8,9,0];
 const rowTwo = ['q','w','e','r','t','y','u','i','o','p'];
+const rowThree = ['z','x','c','v','b','n','m',',','.','?'];
 
-const rowThree = [
-  {'z': 'z'}, {'x':'x'}, {'c':'c'}, {'v':'v'}, {'b':'b'}, {'n':'n'},
-  {'m':'m'}, {'comma': ','}, {'dot': '.'}, {'question': '?'}
-];
-
-const generateButtonsFromObj = (row, element, top, leftStart, leftInc) => {
+const generateButtons = (row, element, top, leftStart, leftInc) => {
   row.forEach(key => {
-    element.innerHTML += `
-      <div class="key" id=${Object.keys(key)} style="top:${top}%;left:${leftStart}%">
-        ${key[Object.keys(key)]}
+    two.innerHTML += `
+      <div class="key" id=${key} style="top:${top}%;left:${leftStart}%">
+        ${key}
         <div class="key-stem"></div>
       </div>
     `
-    leftStart+= leftInc;
-
+    leftStart += leftInc;
   })
 }
 
-generateButtonsFromObj(rowOne, one, 0, -2, 10.6);
-generateButtonsFromObj(rowThree, three, 54, 2, 9.7);
+generateButtons(rowOne, one, 0, -2, 10.6);
+generateButtons(rowTwo, two, 26, 0, 10.2);
+generateButtons(rowThree, three, 54, 2, 9.7);
 
-let topConst = 26;
-let left = 0;
-const leftInc = 10.2;
+const keys = [...document.querySelectorAll('.key')];
 
-rowTwo.forEach(key => {
-  two.innerHTML += `
-    <div class="key" id=${key} style="top:${topConst}%;left:${left}%">
-      ${key}
-      <div class="key-stem"></div>
-    </div>
-  `
-  left += leftInc;
-})
-
-
-
-
-/*
 keys.forEach(key => {
   key.addEventListener('click', function(){
-    let currentTop = document.getElementById(this.id);
-    currentTop.classList.add('moveKeyRow');
-    setTimeout(()=> currentTop.classList.remove('moveKeyRow'), 400);
+    let thisTop = parseInt(this.style.top.replace('%',''));
+    if(canClick){
+      canClick = false;
+      this.style.top = thisTop + 5 + '%';
+      setTimeout(()=> {
+        this.style.top = thisTop + '%';
+        canClick = true;
+      }, 400);
+    }
   })
 })
-
-*/
