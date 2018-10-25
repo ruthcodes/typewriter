2b4dc008df98836a6eef2691bd399d50a954caa3
Ruth Ashleigh <notruth@notruth.com>
2018-10-24 19:46:58 -0700

    Remove magic number, add platen and paper movement

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const RESET_PLATEN = 96;
+
 let canClick = true;
 //page element parents
 const one = document.querySelector('#rowOne');
 const two = document.querySelector('#rowTwo');
 const three = document.querySelector('#rowThree');
 const numberKeys = document.querySelector('#numbers-keys');
+const lever = document.querySelector('.lever');
+const platen = document.querySelector('.platen');
+const paper = document.querySelector('.paper');
+const backPaper = document.querySelector('.nextPaper');
+
 
 //values to generate page element children from
 const nums = [1,2,3,4,5,6,7,8,9,0];
@@ -32,6 +39,7 @@ const keys = [...document.querySelectorAll('.key')];
 
 keys.forEach(key => {
   key.addEventListener('click', function(){
+    //move key down and up
     let thisTop = parseInt(this.style.top.replace('%',''));
     if(canClick){
       canClick = false;
@@ -41,5 +49,35 @@ keys.forEach(key => {
         canClick = true;
       }, 400);
     }
+    //move platen
+    let platenPosition = parseInt(window.getComputedStyle(platen).left.replace('px', ''));
+      if (platenPosition > -214){
+        platen.style.left = platenPosition-=10;
+      } else {
+        //reset platen
+        lever.classList.add('pressLever');
+        setTimeout(()=> lever.classList.remove('pressLever'), 1100)
+        platen.style.left = RESET_PLATEN;
+        //adjust front and back paper
+        adjustPaper();
+      }
   })
+})
+
+const adjustPaper = () => {
+  let frontPaperPos = parseInt(window.getComputedStyle(paper).top.replace('px', ''));
+  let frontPaperHeight = parseInt(window.getComputedStyle(paper).height.replace('px', ''));
+  let backPaperPos = parseInt(window.getComputedStyle(backPaper).top.replace('px', ''));
+  let backPaperHeight = parseInt(window.getComputedStyle(backPaper).height.replace('px', ''));
+  paper.style.top = frontPaperPos-=10;
+  paper.style.height = frontPaperHeight+=10;
+  backPaper.style.top = backPaperPos+=5;
+  backPaper.style.height = backPaperHeight-=5;
+}
+
+lever.addEventListener('click', ()=> {
+  lever.classList.add('pressLever');
+  setTimeout(()=> lever.classList.remove('pressLever'), 1100);
+  platen.style.left = RESET_PLATEN;
+  adjustPaper();
 })
