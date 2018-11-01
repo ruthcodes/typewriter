58ad0747917717e4077f3124d8c7efd4086ffd1b
Ruth Ashleigh <notruth@notruth.com>
2018-11-01 14:00:36 -0700

    Speed up button transition, add keyboard handlers

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const RESET_PLATEN = 96;
+
 let canClick = true;
 let movePlaten = true;
 let textRowCounter = 1;
@@ -13,7 +13,8 @@ const platen = document.querySelector('.platen');
 const paper = document.querySelector('.paper');
 const backPaper = document.querySelector('.nextPaper');
 let text = document.querySelector('#text1');
-
+let buttons = [];
+const RESET_PLATEN = window.getComputedStyle(platen).left.replace('px', '');
 
 //values to generate page element children from
 const nums = [1,2,3,4,5,6,7,8,9,0];
@@ -33,6 +34,46 @@ const generateButtons = (row, element, top, leftStart, leftInc,height=15) => {
   })
 }
 
+const handleInput = (e) => {
+  //move key down and up
+  let thisTop = parseInt(e.style.top.replace('%',''));
+  if(canClick){
+    canClick = false;
+    e.style.top = thisTop + 20 + '%';
+    setTimeout(()=> {
+      e.style.top = thisTop + '%';
+      canClick = true;
+    }, 100);
+  }
+  //move platen
+  let platenPosition = parseInt(window.getComputedStyle(platen).left.replace('px', ''));
+  if (movePlaten){
+    if (platenPosition > -214 && movePlaten){
+      movePlaten = false;
+      platen.style.left = platenPosition-=7;
+      setTimeout(()=> {
+        movePlaten = true;
+      }, 100)
+    } else {
+      //reset platen
+      lever.classList.add('pressLever');
+      setTimeout(()=> lever.classList.remove('pressLever'), 1100)
+      platen.style.left = RESET_PLATEN;
+      //adjust front and back paper
+      adjustPaper();
+      //add new line for text
+      textRowCounter++;
+      let div = document.createElement('div');
+      div.id = `text${textRowCounter}`;
+      div.className = "text";
+      paper.appendChild(div);
+      text = document.querySelector(`#text${textRowCounter}`);
+    }
+    //add char to text
+  text.innerHTML += e.id === 'space' ? ' ' : e.id;
+  }
+}
+
 generateButtons(rowOne, one, 0, -2, 10.6);
 generateButtons(rowTwo, two, 26, 0, 10.2);
 generateButtons(rowThree, three, 54, 2, 9.7);
@@ -41,46 +82,21 @@ generateButtons(nums, numberKeys, 0,1,10,12)
 const keys = [...document.querySelectorAll('.key')];
 
 keys.forEach(key => {
-  key.addEventListener('click', function(){
-    //move key down and up
-    let thisTop = parseInt(this.style.top.replace('%',''));
-    if(canClick){
-      canClick = false;
-      this.style.top = thisTop + 5 + '%';
-      setTimeout(()=> {
-        this.style.top = thisTop + '%';
-        canClick = true;
-      }, 400);
-    }
-    //move platen
-    let platenPosition = parseInt(window.getComputedStyle(platen).left.replace('px', ''));
-    if (movePlaten){
-      if (platenPosition > -214 && movePlaten){
-        movePlaten = false;
-        platen.style.left = platenPosition-=7;
-        setTimeout(()=> {
-          movePlaten = true;
-        }, 300)
-      } else {
-        //reset platen
-        lever.classList.add('pressLever');
-        setTimeout(()=> lever.classList.remove('pressLever'), 1100)
-        platen.style.left = RESET_PLATEN;
-        //adjust front and back paper
-        adjustPaper();
-        //add new line for text
-        textRowCounter++;
-        let div = document.createElement('div');
-        div.id = `text${textRowCounter}`;
-        div.className = "text";
-        paper.appendChild(div);
-        text = document.querySelector(`#text${textRowCounter}`);
-      }
-      //add char to text
-    text.innerHTML += this.id === 'space' ? ' ' : this.id;
-    }
-  })
+  buttons.push(key.id)
+  key.addEventListener('click', (e) => handleInput(e.target));
 })
+
+window.addEventListener('keydown', (e)=> {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+  }
+  if (e.key === ' '){
+    handleInput(document.querySelector('#space'))
+  }
+  if (buttons.includes(e.key)){
+    handleInput(document.querySelector(`#${e.key}`));
+  }
+});
 
 const adjustPaper = () => {
   let frontPaperPos = parseInt(window.getComputedStyle(paper).top.replace('px', ''));
